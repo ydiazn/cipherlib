7cdb36043c4f31161b180561211fdaab67c3433f
Caquarta12 <126199038+Caquarta12@users.noreply.github.com>
2023-07-04 18:41:04 +0100

    Adicionado o metodo vigenere ao Decryptor

--- decryptor/decryption_methods.py
+++ decryptor/decryption_methods.py
@@ -1,14 +1,4 @@
-from hacking import transpositionHacker, affineHacker
-
-
-def decrypt_with_affine(message):
-    print("Decifrando usando a Cifra de Afine...")
-    decrypted_message = affineHacker.hackAffine(message)
-    if decrypted_message is not None:
-        print("Mensagem decifrada usando a Cifra de Afine:")
-        print(decrypted_message)
-    else:
-        print("Falha ao decifrar a mensagem usando a Cifra de Afine.")
+from hacking import transpositionHacker, affineHacker, vigenereHacker
 
 def decrypt_with_transposition(message):
     print("Decifrando usando a Cifra de Transposição...")
@@ -19,8 +9,28 @@ def decrypt_with_transposition(message):
     else:
         print("Falha ao decifrar a mensagem usando a Cifra de Transposição.")
 
+def decrypt_with_affine(message):
+    print("Decifrando usando a Cifra de Afine...")
+    decrypted_message = affineHacker.hackAffine(message)
+    if decrypted_message is not None:
+        print("Mensagem decifrada usando a Cifra de Afine:")
+        print(decrypted_message)
+    else:
+        print("Falha ao decifrar a mensagem usando a Cifra de Afine.")
+
+
+def decrypt_with_vigenere(message):
+    print("Decifrando usando a Cifra de Afine...")
+    decrypted_message = vigenereHacker.hackVigenere(message)
+    if decrypted_message is not None:
+        print("Mensagem decifrada usando a Cifra de Vigenere:")
+        print(decrypted_message)
+    else:
+        print("Falha ao decifrar a mensagem usando a Cifra de Vigenere.")
 
 def decrypt_message(message):
+    decrypt_with_transposition(message)
+    print("-----------------------------------")
     decrypt_with_affine(message)
     print("-----------------------------------")
-    decrypt_with_transposition(message)
+    decrypt_with_vigenere(message)
