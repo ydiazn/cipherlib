f6ae3af594e7771054c536776bd81674cdaed56a
Caquarta12 <126199038+Caquarta12@users.noreply.github.com>
2023-07-04 12:26:02 +0100

    Pasta com os hackings

---
+++ hacking/transpositionHacker.py
@@ -0,0 +1,50 @@
+# Transposition Cipher Hacker
+# http://inventwithpython.com/hacking (BSD Licensed)
+
+from ciphers import transpositionDecrypt
+from utils import detectEnglish, pyperclip
+
+
+def main():
+    # You might want to copy & paste this text from the source code at
+    # http://invpy.com/transpositionHacker.py
+    myMessage = "hdello worl"
+    hackedMessage = hackTransposition(myMessage)
+
+    if hackedMessage == None:
+        print('Failed to hack encryption.')
+    else:
+        print('Copying hacked message to clipboard:')
+        print(hackedMessage)
+        pyperclip.copy(hackedMessage)
+
+
+def hackTransposition(message):
+    print('Hacking...')
+
+    # Python programs can be stopped at any time by pressing Ctrl-C (on
+    # Windows) or Ctrl-D (on Mac and Linux)
+    print('(Press Ctrl-C or Ctrl-D to quit at any time.)')
+
+    # brute-force by looping through every possible key
+    for key in range(1, len(message)):
+        print('Trying key #%s...' % (key))
+
+        decryptedText = transpositionDecrypt.decryptMessage(key, message)
+
+        if detectEnglish.isEnglish(decryptedText):
+            # Check with user to see if the decrypted key has been found.
+            print()
+            print('Possible encryption hack:')
+            print('Key %s: %s' % (key, decryptedText[:100]))
+            print()
+            print('Enter D for done, or just press Enter to continue hacking:')
+            response = input('> ')
+
+            if response.strip().upper().startswith('D'):
+                return decryptedText
+
+    return None
+
+if __name__ == '__main__':
+    main()
