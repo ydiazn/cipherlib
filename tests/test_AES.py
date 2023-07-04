f9e74323e57b26a54e99c09fa1c0a0aedb268745
Florindo Pataca <florindosilili926800020@gmail.com>
2023-07-04 07:19:03 +0100

    Adicionei o arquivo de teste AES

---
+++ tests/test_AES.py
@@ -0,0 +1,22 @@
+import unittest
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad, unpad
+
+from AES import AESCipher
+
+
+class AESCipherTests(unittest.TestCase):
+    def test_encryption_decryption(self):
+        key = get_random_bytes(16)
+        cipher = AESCipher(key)
+
+        plaintext = b'Mensagem secreta'
+        ciphertext = cipher.encrypt(plaintext)
+        decrypted_text = cipher.decrypt(ciphertext)
+
+        self.assertEqual(decrypted_text, plaintext)
+
+
+if __name__ == '__main__':
+    unittest.main()
