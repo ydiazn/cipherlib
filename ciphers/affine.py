232a3f40dc1e53e1a4666f2dd1cf29080ffcd49d
Caquarta12 <126199038+Caquarta12@users.noreply.github.com>
2023-07-09 08:00:36 +0100

    Cifra afim modificada

--- ciphers/affine.py
+++ ciphers/affine.py
@@ -1,29 +1,86 @@
-from .base import BaseCipher
+# Affine Cipher
+
+import sys
+import random
 
 
-LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+from ciphers.base import BaseCipher
+from utils import cryptomath, pyperclip
 
 
-class CesarCipher(BaseCipher):
-    def transform(self, text, key):
-        text = text.upper()
-        transformed = ''
+class AffineCipher(BaseCipher):
+    SYMBOLS = """ !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"""  # note the space at the front
 
-        for symbol in text:
-            index = LETTERS.find(symbol)
+    def __init__(self):
+        self.myMessage = """"f-T))h<RhH)?"""
+        self.myKey = 2023
+        self.myMode = 'encrypt'  # set to 'encrypt' or 'decrypt'
 
-            if index == -1:
-                encrypt_symbol = symbol
+    def main(self):
+        if self.myMode == 'encrypt':
+            translated = self.encryptMessage(self.myKey, self.myMessage)
+        elif self.myMode == 'decrypt':
+            translated = self.decryptMessage(self.myKey, self.myMessage)
+        print('Key: %s' % (self.myKey))
+        print('%sed text:' % (self.myMode.title()))
+        print(translated)
+        pyperclip.copy(translated)
+        print('Full %sed text copied to clipboard.' % (self.myMode))
+
+    def getKeyParts(self, key):
+        keyA = key // len(self.SYMBOLS)
+        keyB = key % len(self.SYMBOLS)
+        return keyA, keyB
+
+    def checkKeys(self, keyA, keyB, mode):
+        if keyA == 1 and mode == 'encrypt':
+            sys.exit('The affine cipher becomes incredibly weak when key A is set to 1. Choose a different key.')
+        if keyB == 0 and mode == 'encrypt':
+            sys.exit('The affine cipher becomes incredibly weak when key B is set to 0. Choose a different key.')
+        if keyA < 0 or keyB < 0 or keyB > len(self.SYMBOLS) - 1:
+            sys.exit('Key A must be greater than 0 and Key B must be between 0 and %s.' % (len(self.SYMBOLS) - 1))
+        if cryptomath.gcd(keyA, len(self.SYMBOLS)) != 1:
+            sys.exit('Key A (%s) and the symbol set size (%s) are not relatively prime. Choose a different key.' % (
+            keyA, len(self.SYMBOLS)))
+
+    def encryptMessage(self, key, message):
+        keyA, keyB = self.getKeyParts(key)
+        self.checkKeys(keyA, keyB, 'encrypt')
+        ciphertext = ''
+        for symbol in message:
+            if symbol in self.SYMBOLS:
+                # encrypt this symbol
+                symIndex = self.SYMBOLS.find(symbol)
+                ciphertext += self.SYMBOLS[(symIndex * keyA + keyB) % len(self.SYMBOLS)]
             else:
-                new_index = (index + key) % len(LETTERS)
-                encrypt_symbol = LETTERS[new_index]
+                ciphertext += symbol  # just append this symbol unencrypted
+        return ciphertext
 
-            transformed += encrypt_symbol
+    def decryptMessage(self, key, message):
+        keyA, keyB = self.getKeyParts(key)
+        self.checkKeys(keyA, keyB, 'decrypt')
+        plaintext = ''
+        modInverseOfKeyA = cryptomath.findModInverse(keyA, len(self.SYMBOLS))
 
-        return transformed
+        for symbol in message:
+            if symbol in self.SYMBOLS:
+                # decrypt this symbol
+                symIndex = self.SYMBOLS.find(symbol)
+                plaintext += self.SYMBOLS[(symIndex - keyB) * modInverseOfKeyA % len(self.SYMBOLS)]
+            else:
+                plaintext += symbol  # just append this symbol undecrypted
+        return plaintext
 
-    def encrypt(self, text):
-        return self.transform(text, self.key)
+    def getRandomKey(self):
+        while True:
+            keyA = random.randint(2, len(self.SYMBOLS))
+            keyB = random.randint(2, len(self.SYMBOLS))
+            if cryptomath.gcd(keyA, len(self.SYMBOLS)) == 1:
+                return keyA * len(self.SYMBOLS) + keyB
 
-    def decrypt(self, text):
-        return self.transform(text, -self.key)
+
+# If affine.py is run (instead of imported as a module) call
+# the main() function.
+if __name__ == '__main__':
+    cipher = AffineCipher()
+    cipher.main()
